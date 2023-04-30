5df43ea581ba9bd040a39a8cc6d82b96e5eccf92
enricadirado <100517394+enricadirado@users.noreply.github.com>
2023-04-30 22:59:12 +0200

    2 files modified

--- index.js
+++ index.js
@@ -2,5 +2,32 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+/*
+verificare che titolo e autore contengano una certa sequenza di caratteri
+- concatenare titolo e autore
+- convertire maiusc in minus
+- verificare presenza stringa
+titolo, autore, stringa: in 3 variabili
+
+var check = titolo.concat(autore).toLowerCase();
+result.innerHTML = checkString(titolo, autore, stringa);
+function checkString(a, b, c) {
+  var check = a.concat(b).toLowerCase().search(c);
+  return check;
+}
+*/
+
+var titoloE = document.getElementById('titolo');
+var autoreE = document.getElementById('autore');
+var stringaE = document.getElementById('stringa');
+var resultE = document.getElementById('result');
+
+var titolo = 'La Divina Commedia';
+var autore = 'Dante Alighieri';
+var stringa = 'div';
+titoloE.innerHTML = 'Titolo: ' + titolo;
+autoreE.innerHTML = 'Autore: ' + autore;
+stringaE.innerHTML = 'Stringa: ' + stringa;
+
+resultE.innerHTML= titolo.concat(autore).toLocaleLowerCase().match(stringa);
